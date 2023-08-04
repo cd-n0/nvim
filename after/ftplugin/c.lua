22f80fe77eb6ba8af856f720eba7772f18c8e57a
cdn0 <59338209+cd-n0@users.noreply.github.com>
2023-08-04 17:14:32 +0300

    use cc and c++ rather than gcc and added compiler flags

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,5 +1,7 @@
---tcc
-vim.keymap.set("n", "<leader><leader>c", "<cmd>term tcc -Wall -Wextra -Werror -g '%' -o '%:r.out' && './%:r.out'<CR>", { silent = true })
+if vim.env.NVIM_COMPILER_FLAGS == nil then vim.fn.setenv("NVIM_COMPILER_FLAGS", " ") end
+    
+-- cc
+vim.keymap.set("n", "<leader><leader>c", "<cmd>term cc -Wall -Wextra -Werror -g -std=c89" .. vim.env.NVIM_COMPILER_FLAGS .. "'%' -o '%:r.out' && './%:r.out'<CR>", { silent = true })
 
 -- tcc run
 vim.keymap.set("n", "<leader><leader>r", "<cmd>term tcc -Wall -Wextra -Werror -run '%' <CR>", { silent = true })
@@ -7,5 +9,9 @@ vim.keymap.set("n", "<leader><leader>r", "<cmd>term tcc -Wall -Wextra -Werror -r
 -- make
 vim.keymap.set("n", "<leader><leader>m", "<cmd>term sh -c 'for f in GNUmakefile makefile Makefile; do if [ -f \"$f\" ]; then TARGET=$(grep \"^TARGET\\s*=\" \"$f\" | cut -d\"=\" -f2- | tr -d \"[:space:]\"); ./$TARGET; break; fi; done'<CR>", { silent = true })
 
--- gcc just for ANSI check
-vim.keymap.set("n", "<leader><leader>g", "<cmd>term gcc -Wall -Wextra -Werror -g -std=c89 '%' -o '%:r.out' && './%:r.out'<CR>", { silent = true })
+-- Keybind to edit the NVIM_COMPILER_FLAGS environment variable
+vim.keymap.set("n", "<leader><leader>f", function () 
+    local flags = vim.fn.input('NVIM_COMPILER_FLAGS: ')
+    vim.fn.setenv("NVIM_COMPILER_FLAGS", flags)
+end
+,{ silent = true })
--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,5 +1,14 @@
--- g++ just for ANSI check
-vim.keymap.set("n", "<leader><leader>c", "<cmd>term g++ -Wall -Wextra -Werror -g -std=c89 '%' -o '%:r.out' && './%:r.out'<CR>", { silent = true })
+if vim.env.NVIM_COMPILER_FLAGS == nil then vim.fn.setenv("NVIM_COMPILER_FLAGS", " ") end
+    
+-- c++
+vim.keymap.set("n", "<leader><leader>c", "<cmd>term cc -Wall -Wextra -Werror -g -std=c++11" .. vim.env.NVIM_COMPILER_FLAGS .. "'%' -o '%:r.out' && './%:r.out'<CR>", { silent = true })
 
 -- make
 vim.keymap.set("n", "<leader><leader>m", "<cmd>term sh -c 'for f in GNUmakefile makefile Makefile; do if [ -f \"$f\" ]; then TARGET=$(grep \"^TARGET\\s*=\" \"$f\" | cut -d\"=\" -f2- | tr -d \"[:space:]\"); ./$TARGET; break; fi; done'<CR>", { silent = true })
+
+-- Keybind to edit the NVIM_COMPILER_FLAGS environment variable
+vim.keymap.set("n", "<leader><leader>f", function () 
+    local flags = vim.fn.input('NVIM_COMPILER_FLAGS: ')
+    vim.fn.setenv("NVIM_COMPILER_FLAGS", flags)
+end
+,{ silent = true })
