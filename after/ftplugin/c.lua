b7c16c982a54c7353fe483b0ac4408882f2926d2
CD-N0 <cdn0@soydev.xyz>
2023-08-04 17:59:55 +0300

    Fixed the compiler flags not working after setting them, working like they were initially

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,7 +1,6 @@
-if vim.env.NVIM_COMPILER_FLAGS == nil then vim.fn.setenv("NVIM_COMPILER_FLAGS", " ") end
-    
+if vim.env.NVIM_COMPILER_FLAGS == nil then vim.fn.setenv("NVIM_COMPILER_FLAGS", "-Wall -Wextra -Werror -g -std=c89 ") end
 -- cc
-vim.keymap.set("n", "<leader><leader>c", "<cmd>term cc -Wall -Wextra -Werror -g -std=c89" .. vim.env.NVIM_COMPILER_FLAGS .. "'%' -o '%:r.out' && './%:r.out'<CR>", { silent = true })
+vim.keymap.set("n", "<leader><leader>c", "<cmd>term cc " .. vim.env.NVIM_COMPILER_FLAGS .. " '%' -o '%:r.out' && './%:r.out'<CR>", { silent = true})
 
 -- tcc run
 vim.keymap.set("n", "<leader><leader>r", "<cmd>term tcc -Wall -Wextra -Werror -run '%' <CR>", { silent = true })
@@ -11,7 +10,9 @@ vim.keymap.set("n", "<leader><leader>m", "<cmd>term sh -c 'for f in GNUmakefile 
 
 -- Keybind to edit the NVIM_COMPILER_FLAGS environment variable
 vim.keymap.set("n", "<leader><leader>f", function () 
-    local flags = vim.fn.input('NVIM_COMPILER_FLAGS: ')
+    flags = vim.env.NVIM_COMPILER_FLAGS
+    flags = vim.fn.input('NVIM_COMPILER_FLAGS: ', flags)
     vim.fn.setenv("NVIM_COMPILER_FLAGS", flags)
+    vim.cmd("source " .. vim.fn.stdpath "config" .. "/after/ftplugin/c.lua")
 end
 ,{ silent = true })
