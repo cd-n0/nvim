389de63dee1bf889c51285e7ebc89781993a9b86
CD-N0 <cdn0@soydev.xyz>
2023-08-04 19:10:34 +0300

    Added flags as the enviroment variable to the tcc -run as well

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -3,7 +3,7 @@ if vim.env.NVIM_COMPILER_FLAGS == nil then vim.fn.setenv("NVIM_COMPILER_FLAGS", 
 vim.keymap.set("n", "<leader><leader>c", "<cmd>term cc " .. vim.env.NVIM_COMPILER_FLAGS .. " '%' -o '%:r.out' && './%:r.out'<CR>", { silent = true})
 
 -- tcc run
-vim.keymap.set("n", "<leader><leader>r", "<cmd>term tcc -Wall -Wextra -Werror -run '%' <CR>", { silent = true })
+vim.keymap.set("n", "<leader><leader>r", "<cmd>term tcc " .. vim.env.NVIM_COMPILER_FLAGS .. " -run '%' <CR>", { silent = true })
 
 -- make
 vim.keymap.set("n", "<leader><leader>m", "<cmd>term sh -c 'for f in GNUmakefile makefile Makefile; do if [ -f \"$f\" ]; then TARGET=$(grep \"^TARGET\\s*=\" \"$f\" | cut -d\"=\" -f2- | tr -d \"[:space:]\"); ./$TARGET; break; fi; done'<CR>", { silent = true })
