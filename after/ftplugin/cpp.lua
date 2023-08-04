b8d95d550bc97db3fb859dae69336bac91d1f5ab
cdn0 <59338209+cd-n0@users.noreply.github.com>
2023-08-04 17:59:55 +0300

    Fixed the compiler flags not working after setting them, working like they were initially

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,4 +1,4 @@
-if vim.env.NVIM_COMPILER_FLAGS == nil then vim.fn.setenv("NVIM_COMPILER_FLAGS", " ") end
+if vim.env.NVIM_COMPILER_FLAGS == nil then vim.fn.setenv("NVIM_COMPILER_FLAGS", "-Wall -Wextra -Werror -g -std=c++ ") end
     
 -- c++
 vim.keymap.set("n", "<leader><leader>c", "<cmd>term cc -Wall -Wextra -Werror -g -std=c++11" .. vim.env.NVIM_COMPILER_FLAGS .. "'%' -o '%:r.out' && './%:r.out'<CR>", { silent = true })
@@ -8,7 +8,9 @@ vim.keymap.set("n", "<leader><leader>m", "<cmd>term sh -c 'for f in GNUmakefile 
 
 -- Keybind to edit the NVIM_COMPILER_FLAGS environment variable
 vim.keymap.set("n", "<leader><leader>f", function () 
-    local flags = vim.fn.input('NVIM_COMPILER_FLAGS: ')
+    flags = vim.env.NVIM_COMPILER_FLAGS
+    flags = vim.fn.input('NVIM_COMPILER_FLAGS: ', flags)
     vim.fn.setenv("NVIM_COMPILER_FLAGS", flags)
+    vim.cmd("source " .. vim.fn.stdpath "config" .. "/after/ftplugin/cpp.lua")
 end
 ,{ silent = true })
