36fd1b7576d6ac0e4b773d74963a99c5456c2dd0
CD-N0 <cdn0@soydev.xyz>
2023-08-25 13:30:50 +0300

    fixed lua-language-server, added jedi-language-server

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -4,7 +4,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'lua-language-server' }
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'lua_ls', 'jedi_language_server' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         -- on_attach = my_custom_on_attach,
