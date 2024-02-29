81b367cbd58b2346cbf942014bc760be17ad55ba
cd-n0 <cd-n0@localhost.localdomain>
2024-02-29 03:09:05 +0300

    More minimal, and bootstrapped

---
+++ lua/cdn0/lsp.lua
@@ -0,0 +1,15 @@
+-- Check if jdtls is installed
+local ensure_jdtls = function()
+    local fn = vim.fn
+    vim.env.PATH = vim.env.PATH .. ':' .. vim.env.HOME .. '/.local/bin/jdtls'
+    local jdtls_installed = fn.executable('jdtls')
+    if jdtls_installed == 0 then
+        -- Run the installation script
+        local install_script = vim.env.HOME .. '/.config/nvim/ls-installers/install-jdtls.sh'
+        vim.fn.system('sh ' .. install_script)
+        return true
+    end
+    return false
+end
+
+local jdtls_installed = ensure_jdtls()
